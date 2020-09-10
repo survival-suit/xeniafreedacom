02ab2319be2c381abba660b240f147a92b7bcb85
survivalsuit <messier83wait@gmail.com>
2020-09-10 23:31:41 +0300

    Добавил новые изображения. Сделал разделы, добавил анимацию.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
-$(document).ready(function(){
- $('.header').height($(window).height());
-})
+/*$(document).ready(function(){
+	 $('.header').height($(window).height());
+
+})*/
+
+$(function(){
+    var header = $('.header').get(0);
+    function resizeHeader() {
+        header.style.height = window.innerHeight + 'px';
+    }
+    $(window).on('resize', resizeHeader);
+    resizeHeader();
+	
+	
+	var about = $('.about').get(0);
+    function resizeAbout() {
+        about.style.height = window.innerHeight + 'px';
+    }
+    $(window).on('resize', resizeAbout);
+    resizeAbout();
+	
+	
+	var consulting = $('.consulting').get(0);
+    function resizeConsulting() {
+        consulting.style.height = window.innerHeight + 'px';
+    }
+    $(window).on('resize', resizeConsulting);
+    resizeConsulting();
+	
+	
+	var courses = $('.courses').get(0);
+    function resizeCourses() {
+        courses.style.height = window.innerHeight + 'px';
+    }
+    $(window).on('resize', resizeCourses);
+    resizeCourses();
+	
+	var books = $('.books').get(0);
+    function resizeBooks() {
+        books.style.height = window.innerHeight + 'px';
+    }
+    $(window).on('resize', resizeBooks);
+    resizeBooks();
+});
+
+	$(".navbar a").click(function(){
+	$("body,html").animate({
+	scrollTop:$("#" + $(this).data('value')).offset().top
+	},1000)
+	})
